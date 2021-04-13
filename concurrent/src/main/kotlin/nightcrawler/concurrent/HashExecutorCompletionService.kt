49d7a86272d11ebdbc73e94c27ca19448799e28e
night-crawler <lilo.panic@gmail.com>
2021-04-13 19:34:55 +0300

    got rid of HashCodeQueueingFuture

--- concurrent/src/main/kotlin/nightcrawler/concurrent/HashExecutorCompletionService.kt
+++ concurrent/src/main/kotlin/nightcrawler/concurrent/HashExecutorCompletionService.kt
@@ -8,7 +8,6 @@ import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import java.util.concurrent.FutureTask
 import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.RunnableFuture
 
 class HashExecutorCompletionService<T : Any?>(
     private val executor: Executor,
@@ -16,29 +15,39 @@ class HashExecutorCompletionService<T : Any?>(
 ) : ExecutorCompletionService<T>(executor, completionQueue) {
 
     override fun submit(task: Callable<T>): Future<T> {
-        val futureTask = HashCodeFutureTask(task)
-        executor.execute(HashCodeQueueingFuture(futureTask, completionQueue))
+        val futureTask = HashCodeFutureTask(task, completionQueue)
+        executor.execute(futureTask)
         return futureTask
     }
 
     override fun submit(task: Runnable, result: T): Future<T> {
-        val futureTask = HashCodeFutureTask(task, result)
-        executor.execute(HashCodeQueueingFuture(futureTask, completionQueue))
+        val futureTask = HashCodeFutureTask(task, result, completionQueue)
+        executor.execute(futureTask)
         return futureTask
     }
 
     private class HashCodeFutureTask<T> : FutureTask<T> {
+        private var completionQueue: BlockingQueue<Future<T>>
         private var hashCodeValue: Int
         private var callable: Callable<T>
 
-        constructor(callable: Callable<T>) : super(callable) {
+        constructor(callable: Callable<T>, completionQueue: BlockingQueue<Future<T>>) : super(callable) {
             this.hashCodeValue = callable.hashCode()
             this.callable = callable
+            this.completionQueue = completionQueue
         }
 
-        constructor(runnable: Runnable, result: T) : super(runnable, result) {
+        constructor(runnable: Runnable, result: T, completionQueue: BlockingQueue<Future<T>>) : super(
+            runnable,
+            result
+        ) {
             this.hashCodeValue = runnable.hashCode()
             this.callable = Executors.callable(runnable, result)
+            this.completionQueue = completionQueue
+        }
+
+        override fun done() {
+            completionQueue.add(this)
         }
 
         override fun hashCode(): Int = hashCodeValue
@@ -47,29 +56,6 @@ class HashExecutorCompletionService<T : Any?>(
             if (other !is HashCodeFutureTask<*>) return false
 
             if (callable != other.callable) return false
-
-            return true
-        }
-    }
-
-    private class HashCodeQueueingFuture<V>(
-        private val runnableFuture: RunnableFuture<V>,
-        private val completionQueue: BlockingQueue<Future<V>>
-    ) : FutureTask<Void>(runnableFuture, null) {
-        override fun done() {
-            completionQueue.add(runnableFuture)
-        }
-
-        override fun hashCode(): Int {
-            return runnableFuture.hashCode()
-        }
-
-        override fun equals(other: Any?): Boolean {
-            if (this === other) return true
-            if (other !is HashCodeQueueingFuture<*>) return false
-
-            if (runnableFuture != other.runnableFuture) return false
-            if (completionQueue != other.completionQueue) return false
 
             return true
         }
